30060ade430f84e0c1bde55ac8d875134d64a4c8
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-10 13:14:51 +0100

    move items into separate classes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,82 +5,52 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def decrease_sell_in(self, item, amount):
-        item.sell_in -= amount
+    def decrease_sell_in(self, amount):
+        self.sell_in -= amount
 
-    def decrease_quality(self, item, amount):
+    def decrease_quality(self, amount):
         min_quality = 0
-        item.quality -= amount
-        if item.quality < min_quality:
-            item.quality = min_quality
+        self.quality -= amount
+        if self.quality < min_quality:
+            self.quality = min_quality
         
 
-    def increase_quality(self, item, amount):
+    def increase_quality(self, amount):
         max_quality = 50
-        if item.quality < max_quality:
-            item.quality += amount
+        if self.quality < max_quality:
+            self.quality += amount
             return
-        item.quality = max_quality
+        self.quality = max_quality
 
-    def is_past_sell_by_date(self, date):
+    def is_past_sell_by_date(self):
         final_day = 0
-        return date < final_day
+        return self.sell_in < final_day
 
-    def process_brie(self, item):
-        self.decrease_sell_in(item, 1)
-
-        if self.is_past_sell_by_date(item.sell_in):
-            self.increase_quality(item, 2)
-        else:
-            self.increase_quality(item, 1) 
-
-    def process_sulfuras(self, item):
-        return 
-    
-    def process_ticket(self, item):
-        thresholds = {
-            'first': 11,
-            'last': 6
-        }
-        self.increase_quality(item, 1)           
-
-        if item.sell_in < thresholds['first']:
-            self.increase_quality(item, 1)
-
-        if item.sell_in < thresholds['last']:
-            self.increase_quality(item, 1)
-
-        self.decrease_sell_in(item, 1)
-        if self.is_past_sell_by_date(item.sell_in):
-            self.decrease_quality(item, item.quality)
-
-
-    def process_conjured(self, item):
-        self.decrease_sell_in(item, 1)
-        self.decrease_quality(item, 2)
-
-    def process_default(self, item):
-        self.decrease_sell_in(item, 1)
+    def choose_and_convert_item(self, item):
+        match item.name:
+            case "Aged Brie":
+                converted_item = Brie(item.name, item.sell_in, item.quality)
         
-        if self.is_past_sell_by_date(item.sell_in):
-            self.decrease_quality(item, 2)
-        else:
-            self.decrease_quality(item, 1)
+            case "Sulfuras, Hand of Ragnaros":
+                converted_item = Sulfaras(item.name, item.sell_in, item.quality)
 
-    def process_items(self):
-        items_functions = {
-            'Aged Brie': self.process_brie,
-            'Sulfuras, Hand of Ragnaros': self.process_sulfuras,
-            'Backstage passes to a TAFKAL80ETC concert': self.process_ticket,
-            'Conjured Mana Cake': self.process_conjured,
-            'default': self.process_default,
-        }
+            case "Backstage passes to a TAFKAL80ETC concert":
+                converted_item = Ticket(item.name, item.sell_in, item.quality)
 
+            case _:
+                converted_item = Default(item.name, item.sell_in, item.quality)
+        return converted_item
+
+    def process_all_items(self):
         for item in self.items:
-            items_functions.get(item.name, items_functions['default'])(item)
+            converted_item = self.choose_and_convert_item(item)
+            converted_item.process_item()
+            
+            index_of_item = self.items.index(item)
+            self.items[index_of_item] = converted_item
 
     def update_quality(self):
-        self.process_items()
+        self.process_all_items()
 
 
 class Item:
@@ -91,3 +61,59 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    def process_item(self):
+        pass
+
+class Brie(Item, GildedRose):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def process_item(self):
+        self.decrease_sell_in(1)
+
+        if  self.is_past_sell_by_date():
+            self.increase_quality(2)
+        else:
+            self.increase_quality(1)
+
+class Ticket(Item, GildedRose):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def process_item(self):
+        thresholds = {
+            'first': 11,
+            'last': 6
+        }
+        self.increase_quality(1)           
+
+        if self.sell_in < thresholds['first']:
+            self.increase_quality(1)
+
+        if self.sell_in < thresholds['last']:
+            self.increase_quality(1)
+
+        self.decrease_sell_in(1)
+        if self.is_past_sell_by_date():
+            self.decrease_quality(self.quality)
+
+
+class Sulfaras(Item, GildedRose):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def process_item(self):
+        pass
+
+class Default(Item, GildedRose):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def process_item(self):
+        self.decrease_sell_in(1)
+        
+        if self.is_past_sell_by_date():
+            self.decrease_quality(2)
+        else:
+            self.decrease_quality(1)
